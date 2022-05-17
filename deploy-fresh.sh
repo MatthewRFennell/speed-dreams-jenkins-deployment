5c8e280f9746613511356e0f00d84f03aa319bdf
Matthew Fennell <mf2017@ic.ac.uk>
2022-05-17 21:28:58 +0100

    Add Jenkins agent configuration
    This commit introduces the configuration for the Jenkins agent, which
    will run the build. From this point onwards, running the deployment
    script should produce a fully working Jenkins server.

--- deploy-fresh.sh
+++ deploy-fresh.sh
@@ -8,6 +8,6 @@ controller_floating_ip="$(terraform output -raw jenkins_controller_floating_ip)"
 agent_floating_ip="$(terraform output -raw jenkins_agent_floating_ip)"
 cd ../ansible
 sed -i "2s/.*/${controller_static_ip}/" inventory/hosts
-sed -i "5s/.*/${agent_static_ip}/" inventory/hosts
-sleep 10
+sed -i "5s/.*/${agent_floating_ip}/" inventory/hosts
+sleep 20
 ansible-playbook -i inventory/hosts playbook.yml --extra-vars "jenkins_controller_floating_ip=${controller_floating_ip} jenkins_agent_floating_ip=${agent_floating_ip}"
